b4e29ae3fa1bade6d86407bb95fd406fe63e5475
Tide Tran <tide.t@itcgroup.io>
2023-05-07 13:44:25 +0700

    07/05-2

--- src/components/Controller/index.tsx
+++ src/components/Controller/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import style from "@/styles/controller.module.css";
 import Header from "../Header";
 import ControllerOutSide from "../ControllerOutSide";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "@/store";
 import ControllerInSide from "../ControllerInSide";
 import LateralMenu from "../LateralMenu";
--- src/components/Sound/index.tsx
+++ src/components/Sound/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { RootState } from "@/store";
 import { useSelector } from "react-redux";
-import ReactAudioPlayer from "react-audio-player";
 import SoundItem from "./SoundItem";
 export interface ISoundProps {}
 
@@ -18,39 +17,55 @@ export default function Sound(props: ISoundProps) {
   } = useSelector((state: RootState) => state.sound);
   return (
     <div>
-      <SoundItem
-        src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/window_rain.mp3"
-        volume={rain}
-      />
-      <SoundItem
-        src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/city_traffic.mp3"
-        volume={traffic}
-      />
-      <SoundItem
-        src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/keyboard.mp3"
-        volume={keyboard}
-      />
-      <SoundItem
-        src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/people_talk_inside.mp3"
-        volume={people}
-      />
+      {rain != 0 && (
+        <SoundItem
+          src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/window_rain.mp3"
+          volume={rain}
+        />
+      )}
+      {traffic != 0 && (
+        <SoundItem
+          src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/city_traffic.mp3"
+          volume={traffic}
+        />
+      )}
+      {keyboard != 0 && (
+        <SoundItem
+          src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/keyboard.mp3"
+          volume={keyboard}
+        />
+      )}
+      {people != 0 && (
+        <SoundItem
+          src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/people_talk_inside.mp3"
+          volume={people}
+        />
+      )}
 
-      <SoundItem
-        src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/train.mp3"
-        volume={train}
-      />
-      <SoundItem
-        src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/thunders.mp3"
-        volume={thunder}
-      />
-      <SoundItem
-        src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/forest_night.mp3"
-        volume={forest_night}
-      />
-      <SoundItem
-        src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/campfire.mp3"
-        volume={campfire}
-      />
+      {train != 0 && (
+        <SoundItem
+          src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/train.mp3"
+          volume={train}
+        />
+      )}
+      {thunder != 0 && (
+        <SoundItem
+          src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/thunders.mp3"
+          volume={thunder}
+        />
+      )}
+      {forest_night != 0 && (
+        <SoundItem
+          src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/forest_night.mp3"
+          volume={forest_night}
+        />
+      )}
+      {campfire != 0 && (
+        <SoundItem
+          src="https://lofico.nyc3.cdn.digitaloceanspaces.com/effects/campfire.mp3"
+          volume={campfire}
+        />
+      )}
     </div>
   );
 }
